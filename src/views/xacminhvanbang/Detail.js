4a48c708250bee924a13f5f1e8b0623bf100029d
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2023-10-26 09:19:45 +0700

    Update Detail.js

--- src/views/xacminhvanbang/Detail.js
+++ src/views/xacminhvanbang/Detail.js
@@ -260,7 +260,7 @@ const Detail = () => {
           <InputForm1 formik={formik} xs={3} label={t('hocsinh.field.nation')} name="danToc" isDisabled />
         </Grid>
         <Grid xs={12} item container spacing={2}>
-          <InputForm1 formik={formik} xs={6} label={'hocsinh.label.diaChi'} name="diaChi" isDisabled />
+          <InputForm1 formik={formik} xs={6} label={'Địa chỉ'} name="diaChi" isDisabled />
         </Grid>
         <Grid item container spacing={1} xs={12} mt={4} alignItems={'center'}>
           <Grid item>
